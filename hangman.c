d6b56b855023864771ebcf89537b034fe6fa3cf0
Julian Wise <julianadamwise@gmail.com>
2015-07-07 14:52:21 +0200

    Initial board layout

--- hangman.c
+++ hangman.c
@@ -1,113 +1,57 @@
-//Hangman Game programmed by Julian Wise following 'Programming in C with Ease'
-
 #include "stdio.h"
-#include "string.h"
-#include "stdlib.h"
+//Hangman
+
+/*
+int board[25] = {
+	:,  :, :, :, :,
+	:, O,-,X, :,
+	:, X, -, -, :,
+	:, -, -,  -, :,
+	:,  :,  :, :, :,
+}
+*/
+
+const NOUGHTS = 1;
+const CROSSES = 2;
+const BORDER = 3;
+const EMPTY = 0;
+
+const int ConvertTo25[9] = {
+	6,7,8,
+	11,12,13,
+	16,17,18
+};
+
+void InitialiseBoard(int *board) { //uses a pointer to our array
+	
+	//Boarder Squares
+	int index = 0;
+	for(index = 0; index < 25; ++index) {
+		board[index] = BORDER;
+	}
+	//Blank Squares
+	for(index = 0; index < 9; ++index) {
+		board[ConvertTo25[index]] = EMPTY;
+	}
+}
+
+void PrintBoard(const int *board) {
+	int index = 0;
+	printf("\nBoard:\n");
+	for(index = 0; index < 25; ++index) {
+		if(index != 0 && index%5==0){
+			printf("\n");
+		}
+		printf("%4d", board[index]);
+	}
+		printf("\n");
+}
+
 
 int main(){
 	
-	srand(time(NULL));
-	
-	char guessWords[][16] = {
-		"green",
-		"yellow",
-		"purple",
-		"window",
-		"linux",
-		"apple",
-	};
-	
-	int randomIndex = rand() % 6; //Translate the random number into the count of options we have
-	int loopIndex = 0; //the index where each of the words is
-	
-	int numLives = 5;
-	int numCorrect = 0;
-	int oldCorrect = 0;
-	
-	int lengthOfWord = strlen(guessWords[randomIndex]);
-	
-	// Letter indexes are changed from 0 to 1 when letters are correctly guessed
-	//				     0 1 2 3 4
-	//				     a p p  l e
-	int letterGuessed[8] = {0,0,0,0,0,0,0,0};
-	
-	int quit = 0;
-	
-	loopIndex = 0;
-	
-	char guess[16];
-	char letterEntered;
-	
-	
-	printf("guessWords: %s randomIndex : %d lengthOfWord: %d\n",
-		guessWords[randomIndex], randomIndex, lengthOfWord);
-	
-	//Game Loop
-	while (numCorrect < lengthOfWord){
-		
-		printf("\n\nNew Turn....\nHangman Word: ");
-		
-		for( loopIndex = 0; loopIndex < lengthOfWord; loopIndex++ ){
-			
-			if(letterGuessed[loopIndex] == 1) {
-				printf("%c", guessWords[randomIndex][loopIndex]);
-			} else {
-				printf("-");
-			}
-		}
-		
-		
-		printf("\n\nNumber correct so far: %d\n", numCorrect); 
-		printf("\nEnter a guess letter: ");
-		fgets(guess, 16, stdin);
-		
-		if( strncmp(guess, "quit" , 4 ) == 0 ) {
-			quit = 1;
-			break;
-		}
-		
-		letterEntered = guess[0];
-		
-		printf("\nLetter Entered: %c\n", letterEntered);
-		
-		oldCorrect = numCorrect; //Guess total comparison
-		
-		for( loopIndex = 0; loopIndex < lengthOfWord; loopIndex++){
-			
-			if( letterGuessed[loopIndex] == 1 ){ 
-				continue;
-			}
-		
-			if( letterEntered == guessWords[randomIndex][loopIndex]){
-				letterGuessed[loopIndex] = 1; //change in our array to 1, for guessed correctly
-				numCorrect++;
-			}
-		}
-		
-		//Test whether we guessed correctly.
-		if( oldCorrect == numCorrect){
-			numLives--;
-			printf("Sorry, wrong guess, \nLives = %d \n",numLives);
-			
-			if( numLives == 0 ) {
-				break;
-			}
-			
-		} else {
-			printf("\nYou guessed correctly!\n");
-		}
-		
-	} //while loop
-	
-	if( quit == 1 ) {
-		printf("\nThe user quit early\n");
-	} else if(numLives == 0) {
-		printf( "Sorry you lose, the word was: %s\n",guessWords[randomIndex]);
-	} else{
-		printf( "\n\nYou win you champ!\n");
-	}
-	
-	
+	int board[25];
+	InitialiseBoard(&board[0]); //reference address for pointers as first element of array
+	PrintBoard(&board[0]);
 	return 0;
-
 }
